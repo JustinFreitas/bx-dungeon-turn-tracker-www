cbf35ff9b0e307c2208576c45c5774835e310fe3
JustinFreitas <justin.s.freitas@me.com>
2026-06-06 16:18:41 -0600

    chore: explicitly set PM2 to fork mode for state consistency

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,13 +3,14 @@ module.exports = {
     name: "bx-dungeon-turn",
     script: "./index.js",
     instances: 1,
+    exec_mode: "fork", // Explicitly use fork mode for state stability
     autorestart: true,
     watch: false,
     max_memory_restart: "200M",
     env: {
       NODE_ENV: "production",
       PORT: 3000,
-      ADMIN_KEY: "dungeon-master" // CHANGE THIS to a strong secret!
+      ADMIN_KEY: "dungeon-master"
     }
   }]
 };
